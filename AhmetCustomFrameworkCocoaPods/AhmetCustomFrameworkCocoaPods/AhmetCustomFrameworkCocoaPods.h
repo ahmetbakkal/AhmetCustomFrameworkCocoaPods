e81c6d3d7e604d3864cf78c57cf8c939cbd0427a
Ahmet BAKKAL <ahmet.bakkal38@gmail.com>
2021-08-04 11:13:12 +0300

    Test framework added.

---
+++ AhmetCustomFrameworkCocoaPods/AhmetCustomFrameworkCocoaPods/AhmetCustomFrameworkCocoaPods.h
@@ -0,0 +1,18 @@
+//
+//  AhmetCustomFrameworkCocoaPods.h
+//  AhmetCustomFrameworkCocoaPods
+//
+//  Created by Ahmet BAKKAL on 4.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AhmetCustomFrameworkCocoaPods.
+FOUNDATION_EXPORT double AhmetCustomFrameworkCocoaPodsVersionNumber;
+
+//! Project version string for AhmetCustomFrameworkCocoaPods.
+FOUNDATION_EXPORT const unsigned char AhmetCustomFrameworkCocoaPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AhmetCustomFrameworkCocoaPods/PublicHeader.h>
+
+
